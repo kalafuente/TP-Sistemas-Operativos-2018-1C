9b7f7066f08e9c57c564d85b71ba7b34d36f8140
KALAFUENTE <lafuentekaren@gmail.com>
2018-07-15 19:09:45 -0300

    Algoritmo KE

--- Coordinador/src/distribucion.c
+++ Coordinador/src/distribucion.c
@@ -1,4 +1,5 @@
 #include "distribucion.h"
+#include <math.h>
 
 instancia * EquitativeLoad(t_list* listaDeInstancias) {
 
@@ -36,8 +37,81 @@ instancia * LSU(t_list* listaDeInstancias, t_log* logControlDeDistribucion) {
 
 		i++;
 	}
+
 	return instanciaConMenosEntradasUtilizadas;
 }
+void mostrar(t_list* lista){
+	void mostrar(char * elem){
+		printf ("%s", elem);
+	}
+	list_iterate(lista, (void *) mostrar);
+}
+
+instancia * KeyExplicit(t_list* listaDeInstancias, t_log* logControlDeDistribucion, char * clave){
+	log_info(logControlDeDistribucion, "KE");
+	agregarAlfabeto();
+	letrasDeLaInstancia = list_create(); // hay que destruirla
+
+	int bloques = (int) ceil ((double) list_size(alfabeto)/ (double) list_size(listaDeInstancias));
+
+	for (int i=0; i <list_size(listaDeInstancias); i++){
+
+			instanciaYSusCaracteres * elementoNuevo = malloc (sizeof(instanciaYSusCaracteres));
+			elementoNuevo->instancia = list_get(listaDeInstancias,i);
 
 
+			if (list_size(alfabeto)>= bloques)
+				elementoNuevo->caracteres = list_take_and_remove(alfabeto,bloques);
+			else
+				elementoNuevo->caracteres = list_take_and_remove(alfabeto,list_size(alfabeto));
+
+
+			printf("el socket de esta instancia es:::: %d\n",elementoNuevo->instancia->socket);
+			printf("el bloque de esta instancia es:::: \n");
+			mostrar(elementoNuevo->caracteres);
+
+			list_add(letrasDeLaInstancia, elementoNuevo);
+	}
+
+	char inicial [1];
+	obtenerInicial(clave, inicial);
+	printf ("la inicial es %s\n", inicial);
+	return (instanciaQueTieneLaLetra(letrasDeLaInstancia, inicial))->instancia;
+
+}
+
+//------------------FUNCIONES AUXILIARES------------------
+
+void obtenerInicial(char * clave, char inicial [1]){
+	inicial[0] = clave[0];
+}
+
+void agregarAlfabeto(){
+	char l [1];
+	alfabeto = list_create();
+	for(int i =97; i<= 122; i++){
+		l[0] = i;
+		char * letra = string_new();
+		string_append(&letra, &l[0]);
+		list_add(alfabeto, letra);
+	}
+
+}
+
+void destruirAlfabeto(){
+	for(int i =0; i<= 25; i++){
+		char* elemento = list_get(alfabeto,i);
+		free (elemento);
+	}
+	list_destroy(alfabeto);
+}
+
+instanciaYSusCaracteres * instanciaQueTieneLaLetra(t_list* list, void* inicial){
+
+	bool sublistaTieneLetra(instanciaYSusCaracteres * item) {
+		return contieneLaInicial(item->caracteres, inicial);
+	}
+	return list_find(list, (void *) sublistaTieneLetra);
+
+}
 
--- Coordinador/src/distribucion.h
+++ Coordinador/src/distribucion.h
@@ -3,8 +3,19 @@
 
 #include "coordinador.h"
 
-instancia * EquitativeLoad(t_list* listaDeInstancias);
+
+typedef struct instanciaYSusCaracteres {
+	instancia * instancia;
+	t_list* caracteres;
+}instanciaYSusCaracteres;
+
+t_list* alfabeto;
+t_list* letrasDeLaInstancia;
 instancia * LSU(t_list* listaDeInstancias, t_log* logControlDeDistribucion);
-
-
+instancia * EquitativeLoad(t_list* listaDeInstancias);
+instancia * KeyExplicit(t_list* listaDeInstancias, t_log* logControlDeDistribucion, char * clave);
+instanciaYSusCaracteres * instanciaQueTieneLaLetra(t_list* list, void* inicial);
+void obtenerInicial(char * clave, char inicial [1]);
+void agregarAlfabeto();
+void destruirAlfabeto();
 #endif /* DISTRIBUCION_H_ */
